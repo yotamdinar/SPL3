afee8bca53b2ff13a3dc37fa5cbb3ea0fd5ce1db
Shlomi Badihi <shlomibadi100@gmail.com>
2022-01-01 11:58:19 +0200

    "change in bidi/messagingProtocolImp"

--- spl-net/src/main/java/bgu/spl/net/api/bidi/messagingProtocolImp.java
+++ spl-net/src/main/java/bgu/spl/net/api/bidi/messagingProtocolImp.java
@@ -1,9 +1,7 @@
 package bgu.spl.net.api.bidi;
 
 public class messagingProtocolImp {
-<<<<<<< HEAD
-   
-=======
-    private boolean shouldTerminate = false;
->>>>>>> c1318c60e7300af087e2c0f9bbb175c4b7c28c4e
+
+    private boolean shouldTerminateConflict = false;
+
 }
