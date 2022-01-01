e800e79162bf20b35a28333806ee6ea63cf73021
Shlomi Badihi <shlomibadi100@gmail.com>
2022-01-01 12:02:23 +0200

    added methods in bidiMessagingProtocol

--- spl-net/src/main/java/bgu/spl/net/api/bidi/messagingProtocolImp.java
+++ spl-net/src/main/java/bgu/spl/net/api/bidi/messagingProtocolImp.java
@@ -1,7 +1,22 @@
 package bgu.spl.net.api.bidi;
 
-public class messagingProtocolImp {
+public class messagingProtocolImp implements BidiMessagingProtocol<String> {
 
-    private boolean shouldTerminateConflict = false;
+    private boolean shouldTerminate = false;
 
+
+    @Override
+    public void start(int connectionId, Connections<String> connections) {
+
+    }
+
+    @Override
+    public void process(String message) {
+
+    }
+
+    @Override
+    public boolean shouldTerminate() {
+        return shouldTerminate;
+    }
 }
