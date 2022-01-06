b22ac86d913372d107a4589c8a4e945b63fe9466
Yotam <dinaryo@post.bgu.ac.il>
2022-01-06 11:34:51 +0200

    adding teggedusers List to Post

--- spl-net/src/main/java/bgu/spl/net/srv/Post.java
+++ spl-net/src/main/java/bgu/spl/net/srv/Post.java
@@ -1,9 +1,14 @@
 package bgu.spl.net.srv;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Post {
 
     private String publisher;
     private String postContent;
+
+    private List<String> taggedUsernames;
 
     public Post(String post_publisher, String post_content){
         this.publisher = post_publisher;
@@ -16,5 +21,28 @@ public class Post {
 
     public String getPostContent() {
         return postContent;
+    }
+
+    public List<String> getTaggedUsernames(){
+        return taggedUsernames;
+    }
+
+    public void parseTaggedUsernames(){
+        List<String> taggedUsernames = new LinkedList<>();
+        String content = getPostContent();
+        while (content.indexOf('@') >= 0){  //abc@yotam @shlomi ab@xavi
+            content = content.substring(content.indexOf('@')+1);   //yotam @shlomi ab@xavi
+            int firstSpaceafterTag = content.indexOf(' '); //=5
+            if (firstSpaceafterTag < 0)
+                taggedUsernames.add(content.substring(0)); //xavi)
+            else {
+                String username = content.substring(0, firstSpaceafterTag); //yotam
+                System.out.println(username);
+                taggedUsernames.add(username);
+            }
+            if (firstSpaceafterTag < 0 || firstSpaceafterTag + 1 == content.length())
+                break;
+            content = content.substring(firstSpaceafterTag+1);
+        }
     }
 }
