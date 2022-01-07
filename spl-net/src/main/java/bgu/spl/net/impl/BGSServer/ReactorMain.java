2b09b8efc958806ff0c5ef0ff76e399ffda54ec0
Yotam <dinaryo@post.bgu.ac.il>
2022-01-07 17:30:39 +0200

    a

--- spl-net/src/main/java/bgu/spl/net/impl/BGSServer/ReactorMain.java
+++ spl-net/src/main/java/bgu/spl/net/impl/BGSServer/ReactorMain.java
@@ -2,19 +2,12 @@ package bgu.spl.net.impl.BGSServer;
 
 import bgu.spl.net.api.bidi.MessageEncoderDecoderImp;
 import bgu.spl.net.api.bidi.messagingProtocolImp;
-import bgu.spl.net.srv.DataBase;
 import bgu.spl.net.srv.Server;
 
-public class TPCMain {
-
+public class ReactorMain {
     public static void main(String[] args) {
-        Server<String> server = Server.threadPerClient(
+        Server<String> server = Server.reactor(Integer.parseInt(args[1]),
                 Integer.parseInt(args[0]), () -> new messagingProtocolImp(), () -> new MessageEncoderDecoderImp());
         server.serve();
     }
-/*    public static void main(String[] args) {
-        Server<String> server = Server.threadPerClient(
-                7777, () -> new messagingProtocolImp(), () -> new MessageEncoderDecoderImp());
-        server.serve();
-    }*/
 }
